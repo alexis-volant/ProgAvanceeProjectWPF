30255573171f8019b190ede5fd6b8829ae6a8435
Rémi <remi.martens@condorcet.be>
2022-06-27 22:33:18 +0200

    Ajout de la classe métier Message

---
+++ ProgAvanceeProjectWPF/Metier/Message.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Message
+{
+    private Guid idMessage;
+    private string obj;
+    private string content;
+    private Treasurer treasurer;
+    private Member member;
+
+    public Message(Guid idMessage, string obj, string content, Treasurer treasurer, Member member)
+    {
+        this.idMessage = idMessage;
+        this.obj = obj;
+        this.content = content;
+        this.treasurer = treasurer;
+        this.member = member;
+    }
+
+
+    public Guid IdMessage
+    {
+        get { return idMessage; }
+        set { idMessage = value; }
+    }
+
+    public string Obj
+    {
+        get { return obj; }
+        set { obj = value; }
+    }
+
+    public string Content
+    {
+        get { return content; }
+        set { content = value; }
+    }
+
+    public Treasurer Treasurer
+    {
+        get { return treasurer; }
+        set { treasurer = value; }
+    }
+
+    public Member Member
+    {
+        get { return member; }
+        set { member = value; }
+    }
+}
+
