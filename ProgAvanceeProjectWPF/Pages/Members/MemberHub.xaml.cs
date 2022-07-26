9f71f7e8ab17febc7aa186dbaadff3727e54effd
Alexis <alexis.volant@condorcet.be>
2022-07-26 21:09:26 +0200

    navigation de page dans un frame (partie member)

---
+++ ProgAvanceeProjectWPF/Pages/Members/MemberHub.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgAvanceeProjectWPF.Pages.Member
+{
+    /// <summary>
+    /// Logique d'interaction pour Page1.xaml
+    /// </summary>
+    /// 
+ 
+    public partial class MemberHub : Page
+    {
+        public MemberHub()
+        {
+            InitializeComponent();
+        }
+                
+        private void GoToBikeBtn(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Content = null;
+            NavigationService.Navigate(new Uri("../Pages/Member/MemberBike.xaml", UriKind.Relative));
+        }
+    }
+}
