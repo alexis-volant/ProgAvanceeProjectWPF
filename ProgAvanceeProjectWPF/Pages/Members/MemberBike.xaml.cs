95eb69b021a592f6aa1aad19982d7115a30aee6b
alexis-volant <alexis.volant@condorcet.be>
2022-08-07 22:22:32 +0200

    Adding bike in windows

--- ProgAvanceeProjectWPF/Pages/Members/MemberBike.xaml.cs
+++ ProgAvanceeProjectWPF/Pages/Members/MemberBike.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgAvanceeProjectWPF.Pages.Members.Windows;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -34,69 +35,26 @@ namespace ProgAvanceeProjectWPF.Pages.Members
 
             List<Bike> bikes = bike.GetBikesByMember(m);
             BikesGrid.ItemsSource = bikes;
-            List<string> CatNamelist = new List<string>();
-            List<Category> categories = category.GetAllCategories();
 
-            foreach (Category c in categories)
-            {
-                CatNamelist.Add(c.NameCategory);
-            }
-
-
-            AddType.ItemsSource = CatNamelist;
-
-            AddGrid.Visibility = Visibility.Hidden;
-            UpdateGrid.Visibility = Visibility.Hidden;
-
+            
             //System.Diagnostics.Debug.WriteLine($"Resp : {m.Name}");
         }
 
 
         private void AddBikeBtn(object sender, RoutedEventArgs e)
         {
+            AddBike addBike = new AddBike(member);
 
-            AddGrid.Visibility = Visibility.Visible;
-            UpdateGrid.Visibility = Visibility.Hidden;
-
-        }
-
-        private void AddBikeValidation(object sender, RoutedEventArgs e)
-        {
-            string AddBikeType;
-            double AddBikeWeigth;
-            double AddBikeLength;
-
-            if (AddType.Equals(""))
-            {
-                MessageBox.Show("Le type ne peut-être vide.");
-                return;
-            }
-
-
-            AddBikeType = AddType.Text;
-
-            AddBikeWeigth = AddWeigth.Text.Length == 0 ? 0 : Convert.ToDouble(AddWeigth.Text);
-
-            AddBikeLength = AddLength.Text.Length == 0 ? 0 : Convert.ToDouble(AddLength.Text);
-
-            bool addStatus = bike.AddBike(AddBikeType, AddBikeWeigth, AddBikeLength, member);
-
-            if (addStatus)
+            addBike.Closed += (ss, ee) =>
             {
                 List<Bike> bikes = bike.GetBikesByMember(member);
                 BikesGrid.ItemsSource = bikes;
+            };
 
-                AddGrid.Visibility = Visibility.Hidden;
-                AddType.Text = String.Empty;
-                AddWeigth.Text = String.Empty;
-                AddLength.Text = String.Empty;
-            }
-            else
-            {
-                MessageBox.Show("Erreur dans l'ajout du velo.");
-            }
-
+            addBike.Show();
         }
+
+        
 
        
 
---
+++ ProgAvanceeProjectWPF/Pages/Members/Windows/AddBike.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProgAvanceeProjectWPF.Pages.Members.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddBike.xaml
+    /// </summary>
+    public partial class AddBike : Window
+    {
+        Bike bike = new Bike();
+
+        Member member = new Member();
+
+        Category category = new Category();
+        public AddBike(Member m)
+        {
+            InitializeComponent();
+            this.member = m;
+
+            List<string> CatNamelist = new List<string>();
+            List<Category> categories = category.GetAllCategories();
+
+            foreach (Category c in categories)
+            {
+                CatNamelist.Add(c.NameCategory);
+            }
+
+            AddType.ItemsSource = CatNamelist;
+
+        }
+
+        private void AddBikeValidation(object sender, RoutedEventArgs e)
+        {
+            string AddBikeType;
+            double AddBikeWeigth;
+            double AddBikeLength;
+
+            if (AddType.Equals(""))
+            {
+                MessageBox.Show("Le type ne peut-être vide.");
+                return;
+            }
+
+
+            AddBikeType = AddType.Text;
+
+            AddBikeWeigth = AddWeigth.Text.Length == 0 ? 0 : Convert.ToDouble(AddWeigth.Text);
+
+            AddBikeLength = AddLength.Text.Length == 0 ? 0 : Convert.ToDouble(AddLength.Text);
+
+            bool addStatus = bike.AddBike(AddBikeType, AddBikeWeigth, AddBikeLength, member);
+
+            if (addStatus)
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Erreur dans l'ajout du velo.");
+                this.Close();
+            }
+
+        }
+
+
+
+
+    }
+}
